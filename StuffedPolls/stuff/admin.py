221516957c9079291442765a6162c295286e0f0f
Ivan <ipenve@gmail.com>
2018-05-15 10:29:02 +0200

    Factions, choicelist and permissions

--- StuffedPolls/stuff/admin.py
+++ StuffedPolls/stuff/admin.py
@@ -1,17 +1,29 @@
 from django.contrib import admin
 
-from .models import AllianceCharacter, HordeCharacter
+from .models import AllianceCharacter, HordeCharacter, Faction
 
 
 class CharacterAdmin(admin.ModelAdmin):
-    list_display = ("character_name", "id", "faction", "race", "character_class", "status", "article_creation_date",
-                    "has_image", "has_info")
+    list_display = ["character_name", "id",  "race", "character_class", "status", "article_creation_date",
+                    "has_image", "has_info"]
     list_filter = ["race"]
     search_fields = ["character_name", "id"]
 
 
+class FactionAdmin(admin.ModelAdmin):
+    list_display = ["faction_name"]
+
+    def has_add_permission(self, request):
+        num_objects = self.model.objects.count()
+        if num_objects >= 2:
+            return False
+        else:
+            return True
+
+
 admin.site.register(AllianceCharacter, CharacterAdmin)
 admin.site.register(HordeCharacter, CharacterAdmin)
+admin.site.register(Faction, FactionAdmin)
 
 # class PageAdmin(admin.ModelAdmin):
 #     list_display = ("title", "id", "article_created_by", "article_creation_date", "has_image")
--- StuffedPolls/stuff/models.py
+++ StuffedPolls/stuff/models.py
@@ -1,18 +1,22 @@
 from django.db import models
 
 
+# class Character(models.Model):
+#     character_name = models.CharField(max_length=100, default="")
+#     character_info = models.CharField(max_length=9999, null=True, blank=True)
+#     faction = models.CharField(max_length=8, choices=Factions, default="")
+#     status = models.CharField(max_length=8, choices=Status_Choices, default="")
+#     character_class = models.CharField(max_length=20, choices=Classes, default="")
+#     article_creation_date = models.DateTimeField('creation_date', null=True)
+#     image = models.ImageField(upload_to='images', null=True, blank=True)
+#     string = "images"
+
+
 Status_Choices = (
     ('Alive', 'Alive'),
     ('Dead', 'Dead'),
     ('Incapacitated', 'Incapacitated'),
     ('Unknown', 'Unknown'),
-)
-
-
-Factions = (
-    ('Choose', 'Choose...'),
-    ('Alliance', 'Alliance'),
-    ('Horde', 'Horde'),
 )
 
 
@@ -52,10 +56,17 @@ Classes = (
 )
 
 
+class Faction(models.Model):
+    faction_name = models.CharField(max_length=8, default="")
+
+    def __str__(self):
+        return self.faction_name
+
+
 class Character(models.Model):
     character_name = models.CharField(max_length=100, default="")
     character_info = models.CharField(max_length=9999, null=True, blank=True)
-    faction = models.CharField(max_length=8, choices=Factions, default="")
+    faction = models.ForeignKey(Faction, on_delete=models.CASCADE, null=True, blank=True)
     status = models.CharField(max_length=8, choices=Status_Choices, default="")
     character_class = models.CharField(max_length=20, choices=Classes, default="")
     article_creation_date = models.DateTimeField('creation_date', null=True)
@@ -66,11 +77,11 @@ class Character(models.Model):
         return self.character_name
 
     def has_image(self):
-        return True if str(self.image).find(self.string) == 0 else False
+        return True if str(self.image).find(self.string) is 0 else False
     has_image.boolean = True
 
     def has_info(self):
-        return True if self.character_info != None else False
+        return True if self.character_info is not None else False
     has_info.boolean = True
 
     class Meta:
